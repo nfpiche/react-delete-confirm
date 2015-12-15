5627bf87b36ff33728002d0bce866475a11d08d9
Nate Piche <nfpiche@mtu.edu>
2015-12-14 20:45:26 -0500

    workingish!

--- index.jsx
+++ index.jsx
@@ -1,11 +1,54 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return { modalOpen: false };
+  },
+
+  openModal: function() {
+    this.setState({ modalOpen: !this.state.modalOpen });
+  },
+
+  closeModal: function() {
+    this.setState({ modalOpen: !this.state.modalOpen });
+  },
+
   render: function() {
     return (
       <div>
-        Delete me
+        {this.renderDeleteButton()}
+        {this.state.modalOpen ? this.renderConfirmModal() : null}
       </div>
     );
+  },
+
+  renderDeleteButton: function() {
+    return (
+      <button onClick={this.openModal}>
+        DELETE {this.props.name}
+      </button>
+    );
+  },
+
+  renderConfirmModal: function() {
+    divStyle = {
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      marginTop: '-75px',
+      marginLeft: '-150px',
+      backgroundColor: 'red',
+      height: '150px',
+      width: '300px',
+      zIndex: '9999'
+    }
+
+    return (
+      <div style={divStyle}>
+        <button onClick={this.closeModal}>
+          CLOSE MODAL
+        </button>
+      </div>
+    )
   }
 });
